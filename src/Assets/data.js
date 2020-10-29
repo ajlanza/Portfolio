ef671dceb9be2bb88f9fc91403b888b0c1ba9392
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-10-29 10:46:19 -0400

    changed about me and descriptions for projects

--- src/Assets/data.js
+++ src/Assets/data.js
@@ -8,7 +8,7 @@ const data = [
       class: 'bigScreen'
     },
     url: 'https://brag.vercel.app/',
-    description: 'An app to help friends keep track of the wagers they make with each other. The site lets registered users request other users as friends. Once a friendship is accepted the two users are able to create and send a wager to their friend. The friend is free to accept or deny the wager and then either person can indicate who has won.',
+    description: 'An app to help friends keep track of the wagers they make with each other. The site lets registered users request other users as friends. Once a friendship is accepted, a user can create and send a wager to their friend. The friend is free to accept or deny the wager, and either person can indicate who has won.',
     codeURL: 'https://github.com/ajlanza/Bragging-Rights',
     developedWith:  'HTML, CSS, JavaScript, React, Node, Express, and PostgresQL'
 
@@ -22,7 +22,7 @@ const data = [
       class: 'bigScreen'
     },
     url: 'https://phone-triage.vercel.app/',
-    description: 'An app designed for users with malfunctioning phones. The site allows for users to browse through previous problems and solutions. If they are unable to find a similar problem to theirs they can log in and post their particular problem. A user can also request that a service technician meet them wherever they are to help them out.',
+    description: 'An app designed for users with malfunctioning phones. The site allows users to browse through previous problems and solutions. If they are unable to find a similar problem to theirs, they can log in and post their particular problem. A user can also request that a service technician meet them wherever they are to help them out.',
     codeURL: 'https://github.com/ajlanza/phone-triage',
     developedWith: 'HTML, CSS, JavaScript, React, Node, Express, and PostgresQL'
   }, 
@@ -35,7 +35,7 @@ const data = [
       class:'bigGet'
     },
     url: 'https://get-out.vercel.app/',
-    description: 'An app that will show the user where an event is located. The user is asked for a city and then displays a list of events for that city. The user can then click on an event and the app will show where the venue is on the map, display a background image of the band playing, and name the venue where the event is being held.',
+    description: 'An app that will show the user where an event is located. The user is asked for a city and receives a list of events for that city. When the user clicks on an event, the app will show where the venue is on the map, display a background image of the event, and name the venue where the event is being held.',
     codeURL: 'https://github.com/ajlanza/getout',
     developedWith: 'HTML, CSS, JavaScript, and jQuery'
   },   
@@ -48,7 +48,7 @@ const data = [
       class: 'bigScreen'
     },
     url: 'https://ajlanza.github.io/quiz/',
-    description: 'An app that quizzes the user one question at a time. Multiple choice answers are given for each question. Checks to make sure user has picked an answer before checking to see if the user is correct. Gives feedback about the correct answer once an answer is submitted. The current question number as well as score is displayed to the user at all times. If the user gets a question correct the background changes to a picture of stained glass, if the user gets a question wrong the background is changed to an image of broken glass. At the end of the quiz the user is given the choice to start the quiz again.',
+    description: 'An app that quizzes the user one question at a time. Multiple choice answers are given for each question. The app gives feedback about the correct answer once an answer is submitted. The current question number and the user\'s score is displayed to the user at all times. When the user gets a question correct, the background changes to a picture of stained glass. If the user gets a question wrong, the background is changed to an image of broken glass. At the end of the quiz, the user is given the option to start the quiz again.',
     codeURL: 'https://github.com/ajlanza/quiz',
     developedWith: 'HTML, CSS, and jQuery'
   }, 
